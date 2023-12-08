2f8a4020de6f4248e77daa3cf59397b80bc0d224
clebervalerio <seu@email.com>
2023-12-08 12:52:05 -0300

    aula 3 projeto

--- src/app/seguranca/autenticar.guard.ts
+++ src/app/seguranca/autenticar.guard.ts
@@ -1,5 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const autenticarGuard: CanActivateFn = (route, state) => {
+  //importar a classe Router
+  const rota = new Router;
+
+  //Validação
+
+  if(localStorage.getItem('email') == undefined){
+    rota.navigateByUrl('/login');
+  }
+  
+  
   return true;
 };
